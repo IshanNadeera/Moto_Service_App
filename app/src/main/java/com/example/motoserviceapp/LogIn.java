9986db014a7bd12f8a17b549d2735be4b336261a
Yomal-Dulanjana <65000757+Yomal-Dulanjana@users.noreply.github.com>
2021-09-25 14:42:43 +0530

    ui add

--- app/src/main/java/com/example/motoserviceapp/LogIn.java
+++ app/src/main/java/com/example/motoserviceapp/LogIn.java
@@ -2,13 +2,30 @@ package com.example.motoserviceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
+
+    Button signUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button btn = findViewById(R.id.signupBttn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LogIn.this,Registration.class));
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/motoserviceapp/Registration.java
+++ app/src/main/java/com/example/motoserviceapp/Registration.java
@@ -2,13 +2,39 @@ package com.example.motoserviceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Registration extends AppCompatActivity {
+
+    private TextView Name,Email,phone,userName,Password;
+    private EditText NameInput,EmailInput,InputPhone,userNameInput,passwordInput;
+
+    private FirebaseAuth mAuth;
+    private Object Registration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Button btn = findViewById(R.id.RegistrationSignInBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Registration.this,LogIn.class));
+            }
+        });
+
+
+
     }
 }
