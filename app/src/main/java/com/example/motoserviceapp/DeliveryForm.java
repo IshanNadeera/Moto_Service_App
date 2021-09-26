4c61f9ccd99e18e17845732755a590bbd434aac4
Yomal-Dulanjana <65000757+Yomal-Dulanjana@users.noreply.github.com>
2021-09-26 16:07:28 +0530

    Delivery form xml,java file add

---
+++ app/src/main/java/com/example/motoserviceapp/DeliveryForm.java
@@ -0,0 +1,14 @@
+package com.example.motoserviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveryForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_form);
+    }
+}
